eb985609a39876860d6e98330b12a064e14f39d5
Korriban <kling.mobil@gmail.com>
2021-10-21 03:37:50 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { PersistenceModule } from 'angular-persistence';
 
 @NgModule({
   declarations: [
